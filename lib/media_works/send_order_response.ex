b2448936a573cec83f8dd662848d832f72f2951b
Chris Bell <cjbell1988@gmail.com>
2016-06-02 17:57:03 -0400

    Add pos_order_id to SendOrderResponse

--- lib/media_works/send_order_response.ex
+++ lib/media_works/send_order_response.ex
@@ -1,5 +1,5 @@
 defmodule MediaWorks.SendOrderResponse do
-  defstruct [:code, :desc]
+  defstruct [:code, :desc, :pos_order_id]
   @valid_response_codes [0, 2]
 
   def success?(%{code: code}) when code in @valid_response_codes, do: true
